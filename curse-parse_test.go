f6208b3455908b16a6d696982b30dec33be7eec0
Oliver Kahrmann <oliver.kahrmann@gmail.com>
2017-01-08 18:55:10 +0100

    Fix copyright year

--- curse-parse_test.go
+++ curse-parse_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright $today.year Oliver Kahrmann
+Copyright 2017 Oliver Kahrmann
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
